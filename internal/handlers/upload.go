e95189ce13277f98d038781398fa5d74106a4536
andresdanfernandez <46586316+andresdanfernandez@users.noreply.github.com>
2025-06-30 22:53:11 -0500

    add PostgreSQL integration

--- internal/handlers/upload.go
+++ internal/handlers/upload.go
@@ -1,10 +1,14 @@
 package handlers
 
 import (
+	"database/sql"
 	"file-upload/internal/services"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq" // Needed to initialize the postgres driver
 )
 
 func UploadHandler(c *gin.Context) {
@@ -25,6 +29,22 @@ func UploadHandler(c *gin.Context) {
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "upload failed"})
 		return
+	}
+
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Println("Failed to connect to DB", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "DB connection failed"})
+		return
+	}
+	defer db.Close()
+
+	_, err = db.Exec(`
+        INSERT INTO files (filename, url, size)
+        VALUES ($1, $2, $3)
+    `, file.Filename, url, file.Size)
+	if err != nil {
+		log.Println("Failed to insert file metadata:", err)
 	}
 
 	c.JSON(http.StatusOK, gin.H{"url": url})
---
+++ internal/models/file.go
@@ -0,0 +1,11 @@
+package models
+
+import "time"
+
+type File struct {
+	ID         int       `json:"id"`
+	Filename   string    `json:"filename"`
+	URL        string    `json:"url"`
+	Size       int64     `json:"size"`
+	UploadedAt time.Time `json:"uploaded_at"`
+}
